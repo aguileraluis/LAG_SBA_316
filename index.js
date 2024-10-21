32df4dacb3373616ef0ee6f414260b61c6892cb8
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-10-21 10:20:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const newPost = document.getElementById("postForm"); 
+
+const allPosts = Array.from(document.querySelectorAll('div.post')); 
+
+const infoMessage = document.getElementById('categories').nextElementSibling; 
+
+console.log(infoMessage); 
+
+console.log(allPosts); 
+
+console.log(newPost); 
